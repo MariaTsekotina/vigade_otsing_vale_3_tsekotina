7e10051e44af91d7afcf6bbfc3b6dbfc4230d625
opilane <opilane@HP-CZC6297B8F>
2022-01-03 10:36:48 +0200

    dopolneno

--- vigade_otsing_vale_3_tsekotina.py
+++ vigade_otsing_vale_3_tsekotina.py
@@ -1,4 +1,5 @@
 from random import *
+s=nol=pos=neg=[]
 def arvud_loendis():
     print("Данные:")
     n=abs(int(input("Сколько целых чисел генерируем в список? => ")))
@@ -6,47 +7,66 @@ def arvud_loendis():
     maxi=int(input("Введите максимальное число диапазона => "))
     if mini>=maxi:
         mini,maxi=vahetus(mini,maxi)
-    generator(n,s,mini,maxi):
+    generaator(n,s,mini,maxi)
     print()
     print("Результаты:")
     print("Полученный список от",mini,"до",maxi,s)
-    sort(s)
+    s.sort()
     print("Отсортированный список",s)
-    jagamine(s,pos,neg,nol):
+    jagamine(s,pos,neg,nol)
     print("Список положительных элементов",pos)
     print("Список отрицательных элементов",neg)
-    print("Список нулевых элементов",null)
-    kesk=keskmine(pos,n):
-    lisamine(s,kesk):
+    print("Список нулевых элементов",nol)
+    kesk=keskmine(pos,n)
+    lisamine(s,kesk)
     print("Среднее положительных:",kesk)
-    kesk=keskmine(neg,n):
-    lisamine(s,kesk):
+    kesk=keskmine(neg)
+    lisamine(s,kesk)
     print("Среднее отрицательных:",kesk)
     print("Добавляем средние в изначалный массив:")
     sort(s)
     print(s)
 
-def vahetus(a,b):
+def vahetus(a:int,b:int):
+    """Kui a>b siis vahetame neid
+    :param int a: Arv, mis on suurem kui b
+    :param int b: Arv, mis on väiksem kui a
+    :rtype:int, int
+    """
     abi=a
     a=b
     b=abi
     return a,b
 
 def generaator(n,loend,a,b):
+    """
+    :param
+    :param
+    
+    """
     for i in range (n):
-    loend(append(randint(a,b)))
+        loend.append(randint(a,b))
     
 
 def jagamine(loend,p,n,nol):
+    """sorteerib positiivseid ja negatiivseid numbreid
+    :param list loend:spisok vseh cisel
+    :param list n:negatiivsete arvude loend
+    :param list p:positiivsete arvude loend
+    :param list nol:nullide loend
+    """
     for el in loend:
         if el>0:
-            p(append(el))
-        elif::
-            n(append(el))
+            p.append(el)
+        elif el<0:
+            n.append(el)
         else:
-            nol(append(el))
+            nol.append(el)
 
 def keskmine(loend,n):
+    """leidke loendist keskmine
+    :param list loend: numbrite loend
+    """
     n=len(loend)
     if n==0:
         kesk=0
@@ -58,7 +78,12 @@ def keskmine(loend,n):
     return kesk
 
 def lisamine(loend,el):
-    loend(append(el))
-    loend(sort())
+    """
+    :param
+    :param
+    
+    """
+    loend.append(el)
+    loend.sort()
 
 arvud_loendis
